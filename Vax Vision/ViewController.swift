c879cf701b475b2457c725f8afd7996a585189d5
Dhanajit kapali <37000235+dhanajitkapali@users.noreply.github.com>
2021-06-23 12:53:27 +0530

    Initial Commit

---
+++ Vax Vision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vax Vision
+//
+//  Created by unthinkable-mac-0025 on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
